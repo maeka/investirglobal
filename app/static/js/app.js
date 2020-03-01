6e31a1d1f58f44a85d1f3e523f46784c8b650e3a
maeka <sk8science@gmail.com>
2020-03-01 12:58:03 -0300

    twelve9

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -19,21 +19,22 @@
 let deferredPrompt;
 const btnAdd = document.querySelector('#btnAdd');
 
-window.addEventListener('beforeinstallprompt', (e) => {
+window.addEventListener('beforeinstallprompt', function(event) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
   console.log('beforeinstallprompt event fired');
   e.preventDefault();
   deferredPrompt = e;
-  btnAdd.style.visibility = 'visible';
+  //btnAdd.style.visibility = 'visible';
 });
 
+// Installation must be done by a user gesture! Here, the button click
 btnAdd.addEventListener('click', (e) => {
-  btnAdd.style.visibility = 'hidden';
-  deferredPrompt = prompt("Aceita instalar o app?");
-
-  console.log(e);
-  //deferredPrompt = e;
-  deferredPrompt = choiceResult;
-  choiceResult
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.visibility = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
     .then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
         console.log('User accepted the A2HS prompt');
