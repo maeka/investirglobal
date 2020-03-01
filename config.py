55d378ff054e77bfc711432390f62e620470ee74
maeka <sk8science@gmail.com>
2020-03-01 17:46:17 -0300

    twelve41

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os.path
 
-basedir = 'https://'+os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
-HOST = "0.0.0.0"
+HOST = "https://0.0.0.0"
 PORT = 8080
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///'+ os.path.join(basedir,'storage.db')
